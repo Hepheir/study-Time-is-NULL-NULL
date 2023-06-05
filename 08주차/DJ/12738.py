eb620e3a9700fdf4522c58d07e8c7c9e64174455
Hepheir <hepheir@gmail.com>
2023-06-05 23:14:35 +0900

    feat(12738): P, DP LIS O(nlogn)

--- 08주차/DJ/12738.py
+++ 08주차/DJ/12738.py
@@ -1,36 +1,18 @@
-import math
-
-MOD = 5000000
-VALUE = 0
-INDEX = 1
+import bisect
 
 N = int(input())
-A = list(zip(map(int, input().split()), range(N)))
-A.sort(key=lambda x: (x[VALUE], -x[INDEX]))
 
-N0 = int(math.pow(2, math.ceil(math.log2(N))) - 1) + 1 # is where array actually starts.
+A = list(map(int, input().split()))
+D = [0] * N
 
-# Uses segment tree for maximum range query
-st = [0] * (N0+N+1)
+D[0] = A[0]
+length = 0
 
 for i in range(N):
-    # i: current index of i-th smallest value
-    # j: offset of original index of i-th smallest value in segment tree
+    if A[i] > D[length]:
+        length += 1
+        D[length] = A[i]
+    else:
+        D[bisect.bisect_left(D, A[i], 0, length)] = A[i]
 
-    # 1. Find maximum length
-    j = A[i][INDEX] + N0
-    m = 0 # maximun value between [0,j]
-    while ~(j-1): # while j is not the left most node of a level.
-        if j & 1: # is right child
-            m = max(st[j-1], m)
-        j //= 2
-
-    # 2. Recalculate LIS
-    j = A[i][INDEX] + N0
-    st[j] = m+1
-    j //= 2
-    while j:
-        st[j] = max(st[2*j], st[2*j+1])
-        j //= 2
-
-print(st[1])
+print(length+1)
